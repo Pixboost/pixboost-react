96de729cc4ce5558cdfbc19b9d86f0d3be9a67c2
Dmitry Pokidov <dpokidov@officeworks.com.au>
2018-07-17 21:26:08 +1000

    * Rewrote example to be more real

---
+++ example/src/Data.js
@@ -0,0 +1,125 @@
+const Cups = [
+  {
+    Name:  'White',
+    Price: 9.99,
+    Image: 'http://www.midday.coffee/assets/cups/blur-2846257__340.jpg',
+  },
+  {
+    Name:  'Cappuccino Modern',
+    Price: 12.99,
+    Image: 'http://www.midday.coffee/assets/cups/cappuccino-593256__340.jpg',
+  },
+  {
+    Name:  'Cappuccino White',
+    Price: 7.99,
+    Image: 'http://www.midday.coffee/assets/cups/cappuccino-756490__340.jpg',
+  },
+  {
+    Name:  'For Geeks!',
+    Price: 19.99,
+    Image: 'http://www.midday.coffee/assets/cups/code-geek-2680204__340.png',
+  },
+  {
+    Name:  'Vintage',
+    Price: 12.99,
+    Image: 'http://www.midday.coffee/assets/cups/coffee-386878__340.jpg',
+  },
+  {
+    Name:  'With Spoon',
+    Price: 8.50,
+    Image: 'http://www.midday.coffee/assets/cups/coffee-777612__340.jpg',
+  },
+  {
+    Name:  'Spider',
+    Price: 12.00,
+    Image: 'http://www.midday.coffee/assets/cups/coffee-2127225__340.jpg',
+  },
+  {
+    Name:  'Classic Cappuccino',
+    Price: 11.99,
+    Image: 'http://www.midday.coffee/assets/cups/coffee-3129995__340.jpg',
+  },
+  {
+    Name:  'Dream Cream',
+    Price: 3.99,
+    Image: 'http://www.midday.coffee/assets/cups/coffee-3139776__340.jpg',
+  },
+  {
+    Name:  'Full of Beans',
+    Price: 10.10,
+    Image: 'http://www.midday.coffee/assets/cups/coffee-3183337__340.jpg',
+  },
+  {
+    Name:  'Signs',
+    Price: 11.99,
+    Image: 'http://www.midday.coffee/assets/cups/coffee-3183729__340.jpg',
+  },
+  {
+    Name:  'Flat White',
+    Price: 15.00,
+    Image: 'http://www.midday.coffee/assets/cups/coffee-3219749__340.jpg',
+  },
+
+  {
+    Name:  'Black in White',
+    Price: 4.99,
+    Image: 'http://www.midday.coffee/assets/cups/coffee-beans-3093228__340.jpg',
+  },
+  {
+    Name:  'Mugs',
+    Price: 11.11,
+    Image: 'http://www.midday.coffee/assets/cups/coffee-mugs-459324__340.jpg',
+  },
+  {
+    Name:  'Christmas Coffee',
+    Price: 17.99,
+    Image: 'http://www.midday.coffee/assets/cups/cup-1891446__340.jpg',
+  },
+  {
+    Name:  'Colours',
+    Price: 9.99,
+    Image: 'http://www.midday.coffee/assets/cups/cup-2315565__340.jpg',
+  },
+  {
+    Name:  'Classic 2',
+    Price: 10.99,
+    Image: 'http://www.midday.coffee/assets/cups/cup-2318315__340.jpg',
+  },
+  {
+    Name:  'Green Dude',
+    Price: 4.99,
+    Image: 'http://www.midday.coffee/assets/cups/cup-3082145__340.jpg',
+  },
+  {
+    Name:  'Red Modern',
+    Price: 12.99,
+    Image: 'http://www.midday.coffee/assets/cups/cup-3117730__340.jpg',
+  },
+  {
+    Name:  'Orange',
+    Price: 15.00,
+    Image: 'http://www.midday.coffee/assets/cups/cup-3183995__340.jpg',
+  },
+  {
+    Name:  'Keep Calm',
+    Price: 10.99,
+    Image: 'http://www.midday.coffee/assets/cups/keep-calm-2816357__340.jpg',
+  },
+  {
+    Name:  'Snowy',
+    Price: 13.13,
+    Image: 'http://www.midday.coffee/assets/cups/mug-3059994__340.jpg',
+  },
+  {
+    Name:  'Smile!',
+    Price: 5.00,
+    Image: 'http://www.midday.coffee/assets/cups/smile-2001662__340.jpg',
+  },
+  {
+    Name:  'Classic 2',
+    Price: 5.99,
+    Image: 'http://www.midday.coffee/assets/cups/tea-cup-2107599__340.jpg',
+  },
+];
+
+export {Cups};
