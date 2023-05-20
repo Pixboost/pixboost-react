74eb1d7f265eaa90d31634ceeb4f4fd21bca3be5
Dmitry Pokidov <dooman87@gmail.com>
2023-05-20 21:53:12 +1000

    New Picture component with High DPI. WIP

--- __tests__/components/Picture.js
+++ __tests__/components/Picture.js
@@ -216,36 +216,4 @@ describe('Picture', () => {
         .toJSON()
     ).toMatchSnapshot();
   });
-
-  it('should render responsive image with hi-DPI support', () => {
-    renderer.create(
-      <Picture alt="YO"
-               config={testConfig}
-               breakpoints={{
-                 sm: {size: '100vw'},
-                 md: {size: '640w'},
-                 lg: {size: '1500w'}
-               }}
-               minSize={300}
-               maxSize={1500}
-               op="resize?size={SIZE}"
-      />
-    );
-  });
-
-  it('should render responsive image with hi-DPI support 2', () => {
-    renderer.create(
-      <Picture alt="YO"
-               config={testConfig}
-               breakpoints={{
-                 sm: {size: '100vh'},
-                 md: {size: '500h'},
-                 lg: {size: '500h'}
-               }}
-               minSize={300}
-               maxSize={1500}
-               op="resize?size=x{SIZE}"
-      />
-    );
-  });
 });
