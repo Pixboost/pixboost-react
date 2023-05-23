9d2796f6ca7b1091b8d426c30276a989c0869a0a
Dmitry Pokidov <dooman87@gmail.com>
2023-05-23 22:21:57 +1000

    Better test coverage, updated babel to version 7

--- __tests__/components/HiDpiPicture.js
+++ __tests__/components/HiDpiPicture.js
@@ -7,26 +7,106 @@ const testConfig = {
   apiKey: 'abc123',
   breakpoints: {
     lg: { media: '(min-width: 990px)' },
-    md: { media: '(min-width: 640px)' },
+    md: { media: '(min-width: 640px) and (max-width: 989.98)' },
     sm: {}
   }
 };
 
 describe ('HiDpiPicture', () => {
-  it('should render responsive image with hi-DPI support', () => {
+  it('should render HiDpiPicture', () => {
     expect(
       renderer.create(
         <HiDpiPicture alt="YO"
                  config={testConfig}
                  breakpoints={{
-                   sm: {size: '100vw', op: 'fit?size={WIDTH}x{HEIGHT}', height: 600},
-                   md: {size: '640w', op: 'resize?size={WIDTH}'},
-                   lg: {size: '1500w', op: 'resize?size={WIDTH}'}
+                   sm: {width: '100vw', op: 'fit?size={WIDTH}x{HEIGHT}', height: 600},
+                   md: {width: '640px', op: 'resize?size={WIDTH}'},
+                   lg: {width: '1500px', op: 'resize?size={WIDTH}'}
                  }}
                  minWidth={300}
                  maxWidth={1500}
                  src="//here.com/logo.png"
         />
       )).toMatchSnapshot();
+  });
+
+  it('should support lazy loading', () => {
+    expect(
+      renderer.create(
+        <HiDpiPicture alt="YO"
+                 config={testConfig}
+                 breakpoints={{
+                   sm: {width: '100vw', op: 'fit?size={WIDTH}x{HEIGHT}', height: 600},
+                   md: {width: '640px', op: 'resize?size={WIDTH}'},
+                   lg: {width: '1500px', op: 'resize?size={WIDTH}'}
+                 }}
+                 minWidth={300}
+                 maxWidth={1500}
+                 src="//here.com/logo.png"
+                 lazy
+        />
+      )).toMatchSnapshot();
+  });
+
+  it('should render HiDpiPicture with extra image attributes', () => {
+    expect(
+      renderer.create(
+        <HiDpiPicture alt="YO"
+                 config={testConfig}
+                 breakpoints={{
+                   sm: {width: '100vw', op: 'fit?size={WIDTH}x{HEIGHT}', height: 600},
+                   md: {width: '640px', op: 'resize?size={WIDTH}'},
+                   lg: {width: '1500px', op: 'resize?size={WIDTH}'}
+                 }}
+                 minWidth={300}
+                 maxWidth={1500}
+                 src="//here.com/logo.png"
+                 imgProps={{
+                   width: 100,
+                   height: 200
+                 }}
+        />
+      )).toMatchSnapshot();
+  });
+
+  it('should support optimise and asis operations', () => {
+    expect(
+      renderer.create(
+        <HiDpiPicture alt="YO"
+                 config={testConfig}
+                 breakpoints={{
+                   sm: {width: '100vw', op: 'optimise'},
+                   md: {width: '640px', op: 'asis'},
+                   lg: {width: '1500px', op: 'resize?size={WIDTH}'}
+                 }}
+                 minWidth={300}
+                 maxWidth={1500}
+                 src="//here.com/logo.png"
+                 imgProps={{
+                   width: 100,
+                   height: 200
+                 }}
+        />
+      )).toMatchSnapshot();
+  });
+
+  it('should error when breakpoint is not defined', () => {
+    expect(() => {
+      renderer.create(
+        <HiDpiPicture alt="YO"
+                      config={testConfig}
+                      breakpoints={{
+                        whatisthis: {width: '100vw', op: 'optimise'},
+                      }}
+                      minWidth={300}
+                      maxWidth={1500}
+                      src="//here.com/logo.png"
+                      imgProps={{
+                        width: 100,
+                        height: 200
+                      }}
+        />
+      )
+    }).toThrow('could not find breakpoint [whatisthis] in the config')
   });
 });
--- src/components/HiDpiPicture.js
+++ src/components/HiDpiPicture.js
@@ -2,34 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {prepareSource} from '../util';
 
-function generateSrcSet(bp, widths, dppx, config, src) {
+function srcSetAttr(bp, widths, dppx, config, src) {
   const imageHeight = dppx * bp.height;
+  const query = `${bp.op.includes('?') ? '&' : '?'}dppx=${dppx}&auth=${config.apiKey}`
+  const scheme = config.domain.includes('//') ? '' : '//';
 
   return widths.reduce((srcSet, w) => {
     const imageWidth = w * dppx
-    return `${srcSet}${srcSet.length != 0 ? ',' : ''}\n${config.domain.includes('//') ? '' : '//'}${
-      config.domain
-    }/api/2/img/${prepareSource(src)}/${bp.op.replace('{WIDTH}', imageWidth).replace('{HEIGHT}', imageHeight)}${
-      bp.op.includes('?') ? '&' : '?'
-    }dppx=${dppx}&auth=${config.apiKey} ${imageWidth}w`
+    const op = `${bp.op.replace('{WIDTH}', imageWidth).replace('{HEIGHT}', imageHeight)}`;
+
+    return `${srcSet}${srcSet.length !== 0 ? ',' : ''}\n${scheme}${config.domain}/api/2/img/${prepareSource(src)}/${op}${query} ${imageWidth}w`
   }, '')
 }
 
 function sizesAttr(breakpoints, config) {
-  return Object.keys(breakpoints).reduce((sizes, bpKey) => {
-    let newSizes = sizes;
-    if (newSizes.length > 0) {
-      newSizes += ', ';
-    }
+  return Object.keys(breakpoints).
+    // A breakpoint without media query must go last
+    sort((bp1, bp2) => {
+      const media1 = config.breakpoints[bp1]['media'];
+      const media2 = config.breakpoints[bp2]['media'];
 
-    if (config.breakpoints[bpKey]['media']) {
-      newSizes += config.breakpoints[bpKey]['media'];
-      newSizes += ' ';
-    }
-    newSizes += breakpoints[bpKey]['size'];
+      if (!media1) {
+        return 1;
+      }
+      if (!media2) {
+        return -1;
+      }
 
-    return newSizes;
-  }, '')
+      return 0;
+    }).
+    reduce((sizes, bpKey) => {
+      let newSizes = sizes;
+      if (newSizes.length > 0) {
+        newSizes += ', ';
+      }
+
+      const mediaQuery = config.breakpoints[bpKey]['media'];
+      if (mediaQuery) {
+        newSizes += `${mediaQuery} `;
+      }
+      newSizes += breakpoints[bpKey]['width'];
+
+      return newSizes;
+    }, '')
 }
 
 function HiDpiPicture({src, alt, config, breakpoints, minWidth, maxWidth, imgProps, lazy}) {
@@ -53,6 +68,10 @@ function HiDpiPicture({src, alt, config, breakpoints, minWidth, maxWidth, imgPro
       const bpConfig = config.breakpoints[bpKey];
       const bp = breakpoints[bpKey];
 
+      if (!bpConfig) {
+        throw new Error(`could not find breakpoint [${bpKey}] in the config`)
+      }
+
       let bpConfigMedia = '';
       if (bpConfig.media) {
         bpConfigMedia = ` and ${bpConfig.media}`;
@@ -60,17 +79,17 @@ function HiDpiPicture({src, alt, config, breakpoints, minWidth, maxWidth, imgPro
       return <React.Fragment key={`${src}${bpKey}`}>
         <source
           media={`(-webkit-min-device-pixel-ratio: 2) and (-webkit-max-device-pixel-ratio: 2.9999)${bpConfigMedia}`}
-          srcSet={generateSrcSet(bp, sizes, 2, config, src)}
+          srcSet={srcSetAttr(bp, sizes, 2, config, src)}
           sizes={sizesAttr(breakpoints, config)}
         />
         <source
           media={`(-webkit-min-device-pixel-ratio: 3)${bpConfigMedia}`}
-          srcSet={generateSrcSet(bp, sizes, 3, config, src)}
+          srcSet={srcSetAttr(bp, sizes, 3, config, src)}
           sizes={sizesAttr(breakpoints, config)}
         />
         <source
           media={`(-webkit-min-device-pixel-ratio: 1) and (-webkit-max-device-pixel-ratio: 1.9999)${bpConfigMedia}`}
-          srcSet={generateSrcSet(bp, sizes, 1, config, src)}
+          srcSet={srcSetAttr(bp, sizes, 1, config, src)}
           sizes={sizesAttr(breakpoints, config)}
         />
       </React.Fragment>
@@ -88,7 +107,11 @@ HiDpiPicture.propTypes = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
   config: PropTypes.object.isRequired,
-  breakpoints: PropTypes.object.isRequired,
+  breakpoints: PropTypes.objectOf(PropTypes.shape({
+    width: PropTypes.string.isRequired,
+    op: PropTypes.string.isRequired,
+    height: PropTypes.number
+  })).isRequired,
   minWidth: PropTypes.number.isRequired,
   maxWidth: PropTypes.number.isRequired,
   imgProps: PropTypes.object,
